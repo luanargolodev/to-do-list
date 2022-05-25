0b15ce52003bc0a2a7bfae03b10c1bb070862dee
Luan Argolo <ipsluan@icloud.com>
2022-05-25 20:56:22 -0300

    feat: Add functionality on the button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import People from './assets/people.svg';
 
 const App = () => {
-  return <div>App</div>;
+  const [comment, setComment] = useState('');
+  const [allComments, setAllComments] = useState([]);
+
+  function handleClick() {
+    setAllComments([...allComments, comment]);
+  }
+
+  return (
+    <div>
+      <img src={People} alt="Pessoas conversando" />
+      <textarea onChange={({ target }) => setComment(target.value)}></textarea>
+      <button onClick={handleClick}>Comentar</button>
+
+      <ul>
+        {allComments.map((comment, index) => (
+          <li key={index}>{comment}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
